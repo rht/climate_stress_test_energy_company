9196a5fd8c1f0d1217191f28feb07171e6b5666e
rht <rhtbot@protonmail.com>
2021-01-08 01:36:26 -0500

    Add alpha_b multiplier

--- climate_stress_test.py
+++ climate_stress_test.py
@@ -205,6 +205,26 @@ brown_energy_percentage = widgets.IntSlider(
 )
 display(brown_energy_percentage)
 
+# alpha_b multiplier
+display(widgets.Label(
+    value='How much does the brown energy company expect its '
+          'cost of maintaining its stock of brown energy to be '
+))
+display(widgets.Label(
+    value='in the future (relative to its current maintenance '
+          'cost of brown energy $\\alpha_b^{default}$)?'
+))
+alpha_b_multiplier = widgets.IntSlider(
+    min=0,
+    max=150,  # TODO find the max
+    step=25,
+    description='Maintenance cost of brown energy relative to $\\alpha_b^{default}$ in percentages (%):',
+    value=100,
+    style=style,
+    layout=widgets.Layout(width='80%')
+)
+display(alpha_b_multiplier)
+
 # Model
 def calculate_cost_g(cg, x, delta_E, Eg):
     return cg * x * delta_E + green_params.value['alpha_g'] * (Eg ** beta)
