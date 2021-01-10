60893f585dd3b140bb6aca9449407728d69de6f6
rht <rhtbot@protonmail.com>
2021-01-09 20:49:17 -0500

    Update dg, db

--- climate_stress_test.py
+++ climate_stress_test.py
@@ -24,8 +24,8 @@ from scipy.optimize import minimize
 from scipy.optimize import Bounds
 
 # Hardcoded params
-dg = 0.075
-db = 0.1  # dg < db
+dg = 0.03  # DEVIATION
+db = 0.04  # dg < db  # DEVIATION
 initial_x = 0.1
 rho_cg = 0.19
 psi = 24.39  # $/tons
